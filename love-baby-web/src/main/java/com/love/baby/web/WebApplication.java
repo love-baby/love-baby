9277570b87793d35cfa2fe1cd24be943049ed789
liangbc <bicong.liang@51zouchuqu.com>
2018-07-24 16:41:30 +0800

    初始化

--- love-baby-web/src/main/java/com/love/baby/web/WebApplication.java
+++ love-baby-web/src/main/java/com/love/baby/web/WebApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author 23770
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableHystrix
+@EnableFeignClients
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);

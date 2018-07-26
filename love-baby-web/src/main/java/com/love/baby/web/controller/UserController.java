aaa22baef256d3a14e327cd9629aebe969cabb2f
liangbc <bicong.liang@51zouchuqu.com>
2018-07-26 20:51:07 +0800

    登录

--- love-baby-web/src/main/java/com/love/baby/web/controller/UserController.java
+++ love-baby-web/src/main/java/com/love/baby/web/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.love.baby.web.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.love.baby.common.bean.User;
 import com.love.baby.common.common.UserSessionCommon;
 import com.love.baby.common.dto.UserDto;
 import com.love.baby.common.exception.SystemException;
-import com.love.baby.web.config.SystemConfig;
 import com.love.baby.web.service.UserService;
 import com.love.baby.web.vo.UserVo;
 import org.apache.commons.lang3.StringUtils;
@@ -14,11 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Map;
 
 /**
  * @author liangbc
@@ -35,45 +28,6 @@ public class UserController {
 
     @Resource
     private UserSessionCommon userSessionCommon;
-
-    /**
-     * 获取所有用户
-     *
-     * @return
-     */
-    @GetMapping("/listAll")
-    public List<UserVo> listAll(@RequestHeader(value = "token") String token) {
-        String uId = userSessionCommon.assertSessionAndGetUid(token);
-        logger.info("获取所有用户 token = {},uId = {}", token, uId);
-        List<UserVo> list = new ArrayList<>();
-        userService.findAll().forEach(user -> list.add(new UserVo(user)));
-        return list;
-    }
-
-
-    /**
-     * 创建用户
-     *
-     * @param userDto
-     * @return
-     */
-    @PutMapping(value = "")
-    public UserDto create(@RequestBody UserDto userDto, @RequestHeader(value = "token") String token) {
-        logger.info("创建用户 userDto = {}", JSON.toJSONString(userDto));
-        userSessionCommon.assertSessionAndGetUid(token);
-        UserDto userDtoOld = userService.findByName(userDto.getName());
-        logger.info("userDtoOld = {}", JSON.toJSONString(userDtoOld));
-        if (userDtoOld != null) {
-            throw new SystemException(500, "用户名已存在");
-        }
-        String id = UUID.randomUUID().toString().replaceAll("-", "");
-        userDto.setId(id);
-        userDto.setStatus(User.Status.normal);
-        userDto.setResourcesPath(SystemConfig.SystemPath + File.separator + LocalDate.now().getYear() + File.separator + LocalDateTime.now().getMonth().getValue() + File.separator + LocalDateTime.now().getDayOfMonth() + File.separator + id);
-        userDto.setCreateTime(Date.from(Instant.now()));
-        userService.create(userDto);
-        return userDto;
-    }
 
 
     /**

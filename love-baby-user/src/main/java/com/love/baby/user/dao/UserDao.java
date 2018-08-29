dbe9a0d1f9f42f507f0c77eea5292c646f6a9708
liangbc <bicong.liang@51zouchuqu.com>
2018-08-29 16:07:44 +0800

    用户管理

--- love-baby-user/src/main/java/com/love/baby/user/dao/UserDao.java
+++ love-baby-user/src/main/java/com/love/baby/user/dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.love.baby.user.dao;
 
-import com.alibaba.fastjson.JSON;
 import com.love.baby.common.bean.User;
 import com.love.baby.common.param.SearchUserParams;
 import com.love.baby.common.util.PageUtil;
@@ -46,48 +45,33 @@ public class UserDao extends BaseDao<User> {
         List params = new ArrayList();
         List paramsCount = new ArrayList();
         if (StringUtils.isNotBlank(searchUserParams.getSearchText())) {
-            logger.info("searchUserParams.getSearchText() = {}", searchUserParams.getSearchText());
             sql += " and name = ?";
             sqlCount += " and name = ?";
             params.add(searchUserParams.getSearchText());
             paramsCount.add(searchUserParams.getSearchText());
         }
         if (StringUtils.isNotBlank(searchUserParams.getDateMin())) {
-            logger.info("searchUserParams.getDateMin() = {}", searchUserParams.getDateMin());
             sql += " and create_time >= ?";
             sqlCount += " and create_time >= ?";
             params.add(searchUserParams.getDateMin() + " 00:00:00");
             paramsCount.add(searchUserParams.getDateMin() + " 00:00:00");
         }
         if (StringUtils.isNotBlank(searchUserParams.getDateMax())) {
-            logger.info("searchUserParams.getDateMax() = {}", searchUserParams.getDateMax());
             sql += " and create_time <= ?";
             sqlCount += " and create_time <= ?";
             params.add(searchUserParams.getDateMax() + " 23:59:59");
             paramsCount.add(searchUserParams.getDateMax() + " 23:59:59");
         }
         if (StringUtils.isNotBlank(searchUserParams.getSortField()) && StringUtils.isNotBlank(searchUserParams.getSort())) {
-            logger.info("searchUserParams.getSortField() = {},searchUserParams.getSort() = {}", searchUserParams.getSortField(), searchUserParams.getSort());
             sql += " order by searchUserParams.getSortField() searchUserParams.getSort()";
         }
         sql += " limit ?,?";
         params.add(cursor);
         params.add(size);
-        Object[] paramsArr = new Object[params.size()];
-        for (int i = 0; i < params.size(); i++) {
-            paramsArr[i]=params.get(i);
-        }
-        Object[] paramsCountArr = new Object[paramsCount.size()];
-        for (int i = 0; i < paramsCount.size(); i++) {
-            paramsCountArr[i]=paramsCount.get(i);
-        }
-
-        logger.info("sql = {},paramsArr = {}", sql, JSON.toJSON(paramsArr));
-        logger.info("sqlCount = {},paramsCountArr = {}", sqlCount, JSON.toJSON(paramsCount.toArray()));
 
         RowMapper<User> rowMapper = BeanPropertyRowMapper.newInstance(entityClass);
-        List<User> list = jdbcTemplate.query(sql, rowMapper, paramsArr);
-        Integer count = jdbcTemplate.queryForObject(sqlCount, Integer.class,paramsCountArr);
+        List<User> list = jdbcTemplate.query(sql, rowMapper, params.toArray());
+        Integer count = jdbcTemplate.queryForObject(sqlCount, Integer.class,paramsCount.toArray());
         PageUtil pageUtil = PageUtil.builder()
                 .data(list)
                 .recordsFiltered(count)

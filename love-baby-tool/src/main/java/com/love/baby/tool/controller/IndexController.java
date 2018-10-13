260b721a581687a985f31628657e3438431dae92
liangbc <bicong.liang@51zouchuqu.com>
2018-10-13 18:18:43 +0800

    工具项目增加

--- love-baby-tool/src/main/java/com/love/baby/tool/controller/IndexController.java
+++ love-baby-tool/src/main/java/com/love/baby/tool/controller/IndexController.java
@@ -1,8 +1,8 @@
 package com.love.baby.tool.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,11 +12,11 @@ import java.util.Map;
  *
  * @author 23770
  */
-@Controller
+@RestController
 @RequestMapping("/index")
 public class IndexController {
 
-    @GetMapping(value = "/index")
+    @GetMapping(value = "")
     public Map index() {
         Map m = new HashMap();
         m.put("a","a");

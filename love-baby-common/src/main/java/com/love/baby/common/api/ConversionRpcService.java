94686ad1713c23f9d20eca7007bb8ba9ed16ca7a
liangbc <bicong.liang@51zouchuqu.com>
2018-10-15 14:29:40 +0800

    整理公共方法

--- love-baby-common/src/main/java/com/love/baby/common/api/ConversionRpcService.java
+++ love-baby-common/src/main/java/com/love/baby/common/api/ConversionRpcService.java
@@ -1,7 +1,11 @@
 package com.love.baby.common.api;
 
 import com.love.baby.common.annotation.NoWapperResponse;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -13,7 +17,7 @@ import java.io.IOException;
  * @author liangbc
  * @date 2018/10/15
  */
-@FeignClient(name = "love-baby-tool", path = "/conversion/rpc")
+@FeignClient(name = "love-baby-tool", path = "/conversion/rpc", configuration = ConversionRpcService.MultipartSupportConfig.class)
 public interface ConversionRpcService {
 
 
@@ -51,6 +55,19 @@ public interface ConversionRpcService {
     @NoWapperResponse
     byte[] apeConversionMp3(@RequestPart(value = "file", required = false) MultipartFile file) throws IOException;
 
+    /**
+     * RPC 上传文件配置文件
+     *
+     * @return
+     */
+
+    @Configuration
+    class MultipartSupportConfig {
+        @Bean
+        public Encoder feignFormEncoder() {
+            return new SpringFormEncoder();
+        }
+    }
 }
 
 
--- love-baby-tool/src/main/java/com/love/baby/tool/ToolApplication.java
+++ love-baby-tool/src/main/java/com/love/baby/tool/ToolApplication.java
@@ -1,13 +1,10 @@
 package com.love.baby.tool;
 
-import feign.codec.Encoder;
-import feign.form.spring.SpringFormEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author 23770
@@ -19,14 +16,5 @@ import org.springframework.context.annotation.Bean;
 public class ToolApplication {
     public static void main(String[] args) {
         SpringApplication.run(ToolApplication.class, args);
-    }
-
-    /**
-     * RPC 上传文件配置文件
-     * @return
-     */
-    @Bean
-    public Encoder feignFormEncoder() {
-        return new SpringFormEncoder();
     }
 }

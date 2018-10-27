cdd53471b452a35653be996cac26adc6053de1f1
liangbc <bicong.liang@51zouchuqu.com>
2018-10-27 17:46:03 +0800

    升级

--- love-baby-common/src/main/java/com/love/baby/common/param/SearchParamsDto.java
+++ love-baby-common/src/main/java/com/love/baby/common/param/SearchParamsDto.java
@@ -1,22 +1,54 @@
 package com.love.baby.common.param;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * @author liangbc
  * @date 2018/8/25
  */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class SearchParamsDto {
     private String dateMin;
     private String dateMax;
     private String searchText;
     private String sort;
     private String sortField;
+
+
+    public String getDateMin() {
+        return dateMin;
+    }
+
+    public void setDateMin(String dateMin) {
+        this.dateMin = dateMin;
+    }
+
+    public String getDateMax() {
+        return dateMax;
+    }
+
+    public void setDateMax(String dateMax) {
+        this.dateMax = dateMax;
+    }
+
+    public String getSearchText() {
+        return searchText;
+    }
+
+    public void setSearchText(String searchText) {
+        this.searchText = searchText;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getSortField() {
+        return sortField;
+    }
+
+    public void setSortField(String sortField) {
+        this.sortField = sortField;
+    }
 }
